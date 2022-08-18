41b8d8fa0f7010eb248309e3053c218f10c6b5bf
Waka758 <88756617+Waka758@users.noreply.github.com>
2022-08-17 19:33:38 -0500

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://75a2dd22.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://74b2e489.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
